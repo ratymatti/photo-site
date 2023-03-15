8fe2e1fbacadccc12713a3067c63c3f3da8b8eb3
Matti Räty <mattiraty@hotmail.com>
2023-03-15 16:34:27 +0200

    added second image category

--- src/modules/images/images.js
+++ src/modules/images/images.js
@@ -3,14 +3,36 @@ import sport2 from '../../images/sport-img/sport2.jpg';
 import sport3 from '../../images/sport-img/sport3.jpg';
 import sport4 from '../../images/sport-img/sport4.jpg';
 
+import flyfishing1 from '../../images/flyfishing-img/flyfishing1.jpg';
+import flyfishing2 from '../../images/flyfishing-img/flyfishing2.jpg';
+import flyfishing3 from '../../images/flyfishing-img/flyfishing3.jpg';
+import flyfishing4 from '../../images/flyfishing-img/flyfishing4.jpg';
+import flyfishing5 from '../../images/flyfishing-img/flyfishing5.jpg';
+import flyfishing6 from '../../images/flyfishing-img/flyfishing6.jpg';
+import flyfishing7 from '../../images/flyfishing-img/flyfishing7.jpg';
+import flyfishing8 from '../../images/flyfishing-img/flyfishing8.jpg';
+
 export const images = [
   {
     category: 'Sports',
-    images: [sport1, sport2, sport3, sport4]
+    images: [
+        sport1,
+        sport2,
+        sport3,
+        sport4
+    ]
   },
   {
-    category: 'Sports2',
-    images: [sport1, sport2, sport3, sport4]
+    category: 'Fishing',
+    images: [
+        flyfishing1,
+        flyfishing2,
+        flyfishing3,
+        flyfishing4,
+        flyfishing5,
+        flyfishing6,
+        flyfishing7,
+        flyfishing8
+    ]
   }
-  // Add more category objects as needed
 ];
