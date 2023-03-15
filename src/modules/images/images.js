286ec868d7e65ace3376e0a7ab4d0e7cc85a52ce
Matti Räty <mattiraty@hotmail.com>
2023-03-15 11:35:58 +0200

    created images module

---
+++ src/modules/images/images.js
@@ -0,0 +1,20 @@
+const images = {
+    Category1: [
+      'image1.jpg',
+      'image2.jpg',
+      'image3.jpg'
+    ],
+    Category2: [
+      'image4.jpg',
+      'image5.jpg',
+      'image6.jpg'
+    ],
+    Category3: [
+      'image7.jpg',
+      'image8.jpg',
+      'image9.jpg'
+    ]
+  };
+  
+  export default images;
+  
