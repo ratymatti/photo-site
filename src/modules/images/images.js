7c0f7a98175b1b9537bdb2a8baa7f57fde0f5c51
Matti Räty <mattiraty@hotmail.com>
2023-04-05 11:34:23 +0300

    imported images

--- src/modules/images/images.js
+++ src/modules/images/images.js
@@ -1,18 +1,138 @@
-import sport1 from '../../images/sport-img/sport1.jpg';
-import sport2 from '../../images/sport-img/sport2.jpg';
-import sport3 from '../../images/sport-img/sport3.jpg';
-import sport4 from '../../images/sport-img/sport4.jpg';
+import news1 from '../../images/news-img/news01.jpg';
+import news2 from '../../images/news-img/news02.jpg';
+import news3 from '../../images/news-img/news03.jpg';
+import news4 from '../../images/news-img/news04.jpg';
+import news5 from '../../images/news-img/news05.jpg';
+import news6 from '../../images/news-img/news06.jpg';
+import news7 from '../../images/news-img/news07.jpg';
+import news8 from '../../images/news-img/news08.jpg';
+import news9 from '../../images/news-img/news09.jpg';
+import news10 from '../../images/news-img/news10.jpg';
+import news11 from '../../images/news-img/news11.jpg';
+import news12 from '../../images/news-img/news12.jpg';
 
-import flyfishing1 from '../../images/flyfishing-img/flyfishing1.jpg';
-import flyfishing2 from '../../images/flyfishing-img/flyfishing2.jpg';
-import flyfishing3 from '../../images/flyfishing-img/flyfishing3.jpg';
-import flyfishing4 from '../../images/flyfishing-img/flyfishing4.jpg';
-import flyfishing5 from '../../images/flyfishing-img/flyfishing5.jpg';
-import flyfishing6 from '../../images/flyfishing-img/flyfishing6.jpg';
-import flyfishing7 from '../../images/flyfishing-img/flyfishing7.jpg';
-import flyfishing8 from '../../images/flyfishing-img/flyfishing8.jpg';
+import people1 from '../../images/people-img/people01.jpg';
+import people2 from '../../images/people-img/people02.jpg';
+import people3 from '../../images/people-img/people03.jpg';
+import people4 from '../../images/people-img/people04.jpg';
+import people5 from '../../images/people-img/people05.jpg';
+import people6 from '../../images/people-img/people06.jpg';
+import people7 from '../../images/people-img/people07.jpg';
+import people8 from '../../images/people-img/people08.jpg';
+import people9 from '../../images/people-img/people09.jpg';
+import people10 from '../../images/people-img/people10.jpg';
+import people11 from '../../images/people-img/people11.jpg';
+import people12 from '../../images/people-img/people12.jpg';
+import people13 from '../../images/people-img/people13.jpg';
+import people14 from '../../images/people-img/people14.jpg';
+
+import event1 from '../../images/event-img/event01.jpg';
+import event2 from '../../images/event-img/event02.jpg';
+import event3 from '../../images/event-img/event03.jpg';
+import event4 from '../../images/event-img/event04.jpg';
+import event5 from '../../images/event-img/event05.jpg';
+import event6 from '../../images/event-img/event06.jpg';
+import event7 from '../../images/event-img/event07.jpg';
+import event8 from '../../images/event-img/event08.jpg';
+import event9 from '../../images/event-img/event09.jpg';
+import event10 from '../../images/event-img/event10.jpg';
+import event11 from '../../images/event-img/event11.jpg';
+import event12 from '../../images/event-img/event12.jpg';
+
+import flyfishing1 from '../../images/flyfishing-img/flyfishing01.jpg';
+import flyfishing2 from '../../images/flyfishing-img/flyfishing02.jpg';
+import flyfishing3 from '../../images/flyfishing-img/flyfishing03.jpg';
+import flyfishing4 from '../../images/flyfishing-img/flyfishing04.jpg';
+import flyfishing5 from '../../images/flyfishing-img/flyfishing05.jpg';
+import flyfishing6 from '../../images/flyfishing-img/flyfishing06.jpg';
+import flyfishing7 from '../../images/flyfishing-img/flyfishing07.jpg';
+import flyfishing8 from '../../images/flyfishing-img/flyfishing08.jpg';
+import flyfishing9 from '../../images/flyfishing-img/flyfishing09.jpg';
+
+import sport1 from '../../images/sport-img/sport01.jpg';
+import sport2 from '../../images/sport-img/sport02.jpg';
+import sport3 from '../../images/sport-img/sport03.jpg';
+import sport4 from '../../images/sport-img/sport04.jpg';
+import sport5 from '../../images/sport-img/sport05.jpg';
+import sport6 from '../../images/sport-img/sport06.jpg';
+import sport7 from '../../images/sport-img/sport07.jpg';
+import sport8 from '../../images/sport-img/sport08.jpg';
+import sport9 from '../../images/sport-img/sport09.jpg';
+import sport10 from '../../images/sport-img/sport10.jpg';
+import sport11 from '../../images/sport-img/sport11.jpg';
+import sport12 from '../../images/sport-img/sport12.jpg';
+import sport13 from '../../images/sport-img/sport13.jpg';
+
+import nature1 from '../../images/nature-img/nature01.jpg';
+import nature2 from '../../images/nature-img/nature02.jpg';
+import nature3 from '../../images/nature-img/nature03.jpg';
+import nature4 from '../../images/nature-img/nature04.jpg';
+import nature5 from '../../images/nature-img/nature05.jpg';
+import nature6 from '../../images/nature-img/nature06.jpg';
+import nature7 from '../../images/nature-img/nature07.jpg';
+import nature8 from '../../images/nature-img/nature08.jpg';
+import nature9 from '../../images/nature-img/nature09.jpg';
+import nature10 from '../../images/nature-img/nature10.jpg';
+import nature11 from '../../images/nature-img/nature11.jpg';
+import nature12 from '../../images/nature-img/nature12.jpg';
+import nature13 from '../../images/nature-img/nature13.jpg';
+import nature14 from '../../images/nature-img/nature14.jpg';
+import nature15 from '../../images/nature-img/nature15.jpg';
 
 export const images = [
+  {
+    category: 'News',
+    images: [
+      news1,
+      news2,
+      news3,
+      news4,
+      news5,
+      news6,
+      news7,
+      news8,
+      news9,
+      news10,
+      news11,
+      news12
+    ]
+  },
+  {
+    category: 'People',
+    images: [
+      people1,
+      people2,
+      people3,
+      people4,
+      people5,
+      people6,
+      people7,
+      people8,
+      people9,
+      people10,
+      people11,
+      people12,
+      people13,
+      people14
+    ]
+  },
+  {
+    category: 'Event',
+    images: [
+      event1,
+      event2,
+      event3,
+      event4,
+      event5,
+      event6,
+      event7,
+      event8,
+      event9,
+      event10,
+      event11,
+      event12
+    ]
+  },
   {
     category: 'Fly Fishing',
     images: [
@@ -23,7 +143,8 @@ export const images = [
         flyfishing5,
         flyfishing6,
         flyfishing7,
-        flyfishing8
+        flyfishing8,
+        flyfishing9
     ]
   },
   {
@@ -32,7 +153,36 @@ export const images = [
         sport1,
         sport2,
         sport3,
-        sport4
+        sport4,
+        sport5,
+        sport6,
+        sport7,
+        sport8,
+        sport9,
+        sport10,
+        sport11,
+        sport12,
+        sport13
+    ]
+  },
+  {
+    category: 'Nature',
+    images: [
+      nature1,
+      nature2,
+      nature3,
+      nature4,
+      nature5,
+      nature6,
+      nature7,
+      nature8,
+      nature9,
+      nature10,
+      nature11,
+      nature12,
+      nature13,
+      nature14,
+      nature15
     ]
   }
 ];
